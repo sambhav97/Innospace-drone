33abaf75015bf3b2e7be44ea968836dc6d901ab2
sambhavjain <s.jain2@student.tue.nl>
2023-06-15 21:06:24 +0200

    Scene 1 updated

---
+++ examples/Scene 1 CO2.py
@@ -0,0 +1,66 @@
+from djitellopy import Tello
+
+tello = Tello()
+tello.connect()
+tello.takeoff()
+tello.go_xyz_speed(0,50,0,10) #X forward, Y left, Z height, Speed
+
+tello.move_down(20)
+
+tello.move_up(30)
+tello.move_down(30)
+tello.move_up(20)
+tello.move_down(20) #10 seconds one 30-20 up down
+
+tello.move_up(30)
+tello.move_down(30)
+tello.move_up(20)
+tello.move_down(20)
+
+tello.go_xyz_speed(0,20,0,10) #2 seconds
+
+tello.move_up(30)
+tello.move_down(30)
+tello.move_up(20)
+tello.move_down(20)
+
+tello.move_up(30)
+tello.move_down(30)
+tello.move_up(20)
+tello.move_down(20)
+
+tello.go_xyz_speed(0,-20,0,10) #2 seconds
+
+tello.move_up(30)
+tello.move_down(30)
+tello.move_up(20)
+tello.move_down(20)
+
+tello.move_up(30)
+tello.move_down(30)
+tello.move_up(20)
+tello.move_down(20)
+
+tello.go_xyz_speed(0,20,0,10)
+
+tello.move_up(30)
+tello.move_down(30)
+tello.move_up(20)
+tello.move_down(20)
+
+tello.move_up(30)
+tello.move_down(30)
+tello.move_up(20)
+tello.move_down(20)
+
+tello.go_xyz_speed(0,-20,0,10)
+
+tello.move_up(30)
+tello.move_down(30)
+tello.move_up(20)
+tello.move_down(20)
+tello.move_up(20)
+tello.move_down(20)
+
+tello.land()
+
---
+++ examples/Scene 1 O2.py
@@ -0,0 +1,64 @@
+from djitellopy import Tello
+
+tello = Tello()
+tello.connect()
+tello.takeoff()
+tello.go_xyz_speed(0,-100,0,10) #X forward, Y left, Z height, Speed
+
+tello.move_up(30)
+tello.move_down(30)
+tello.move_up(20)
+tello.move_down(20) #10 seconds one 30-20 up down
+
+tello.move_up(30)
+tello.move_down(30)
+tello.move_up(20)
+tello.move_down(20)
+
+tello.go_xyz_speed(0,-40,0,10) #2 seconds
+
+tello.move_up(30)
+tello.move_down(30)
+tello.move_up(20)
+tello.move_down(20)
+
+tello.move_up(30)
+tello.move_down(30)
+tello.move_up(20)
+tello.move_down(20)
+
+tello.go_xyz_speed(0,40,0,10) #2 seconds
+
+tello.move_up(30)
+tello.move_down(30)
+tello.move_up(20)
+tello.move_down(20)
+
+tello.move_up(30)
+tello.move_down(30)
+tello.move_up(20)
+tello.move_down(20)
+
+tello.go_xyz_speed(0,-40,0,10)
+
+tello.move_up(30)
+tello.move_down(30)
+tello.move_up(20)
+tello.move_down(20)
+
+tello.move_up(30)
+tello.move_down(30)
+tello.move_up(20)
+tello.move_down(20)
+
+tello.go_xyz_speed(0,40,0,10)
+
+tello.move_up(30)
+tello.move_down(30)
+tello.move_up(20)
+tello.move_down(20)
+tello.move_up(20)
+tello.move_down(20)
+
+tello.land()
+
