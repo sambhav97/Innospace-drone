778c0f5353af7ca21aa0cade2e645b796779cc34
sambhavjain <s.jain2@student.tue.nl>
2023-06-16 11:04:48 +0200

    changes

--- examples/Scene 2 CO2.py
+++ examples/Scene 2 CO2.py
@@ -4,15 +4,17 @@ tello = Tello()
 tello.connect()
 tello.takeoff()
 
-tello.rotate_counter_clockwise(90)
-tello.move_left(100)
-tello.rotate_clockwise(90)
-tello.move_left(100)
-tello.move_forward(100)
-tello.move_right(100)
+tello.move_up(30)
 
-tello.move_back(100)
-tello.move_left(40)
+tello.rotate_counter_clockwise(90)
+tello.move_left(150)
+tello.rotate_clockwise(90)
+tello.move_left(150)
+tello.move_forward(150)s
+tello.move_right(150)
+
+tello.move_back(150)
+tello.move_left(60)
 
 tello.land()
 
